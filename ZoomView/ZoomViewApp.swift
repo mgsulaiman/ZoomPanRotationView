1f3c863063dd04ecfa46ab055086064f1901b12c
mgsulaiman <info@mgsulaiman.com>
2023-04-13 09:42:56 +0300

    Initial Commit

---
+++ ZoomView/ZoomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomViewApp.swift
+//  ZoomView
+//
+//  Created by developer mg on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZoomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
